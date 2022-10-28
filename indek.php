810b8d2cb427bf5343acebb2e60bca0d2162738c
ragakadir123 <ragakadir50@gmail.com>
2022-10-29 01:54:52 +0700

    menambahkan paragraph di indek.php

--- indek.php
+++ indek.php
@@ -10,6 +10,8 @@
 
 <body>
     <h1>Hello World!</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, pariatur earum. Iste officia quo modi labore eum! Exercitationem ex quaerat praesentium, consequatur, nulla unde ullam, repellendus enim id libero magni?</p>
 </body>
 
 </html>
